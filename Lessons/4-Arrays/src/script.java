f9b5674b4181ecb1948f89790673a00087e94821
Hilmi Güner <hilmi.guner@hotmail.com>
2022-10-13 23:30:42 +0300

    BTK Java 5.3 Done

---
+++ Lessons/4-Arrays/src/script.java
@@ -0,0 +1,8 @@
+public class script {
+
+    public static void main(String[] args) {
+        // type[] arr_name = {}
+
+        // Multidimensional Arrays -> type[][]...[] arr_name = {{},{},...,{}}
+    }
+}
