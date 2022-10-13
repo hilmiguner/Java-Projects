dc87461c342618133f391730dc7048b01cd1cadc
Hilmi Güner <hilmi.guner@hotmail.com>
2022-10-13 21:18:05 +0300

    BTK Java 2.3 Done

---
+++ Lessons/3-Loops/src/script.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.locks.Condition;
+
+public class script {
+    public static void main(String[] args) {
+        // for (statement1 ; statement2 ; statement3) {code block}
+
+        // for (type var_name ; arr_name) {code block}
+
+        // while (Condition) {code block}
+
+        // do {code block}while (condition);
+    }
+}
