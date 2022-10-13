ad6a0b3420d34506141a063d108505cc91627115
Hilmi Güner <hilmi.guner@hotmail.com>
2022-10-13 20:46:28 +0300

    BTK Java 3.3 Done

---
+++ Lessons/2-IfElse/src/ifElseAndSwitch.java
@@ -0,0 +1,17 @@
+package src;
+public class ifElseAndSwitch {
+    public static void main(String[] args) {
+        //if (condition) {}
+        //else if (condition) {}
+        //else {}
+
+        // switch(expression) {
+        //     case x:
+        //       break;
+        //     case y:
+        //       break;
+        //     default:
+        //   }
+          
+    }  
+}
